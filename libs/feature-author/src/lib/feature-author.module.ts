5c0d11155bf619019f2e8e08c4dd8cf2102c781f
zelzhan <elzhan.zeinulla@nu.edu.kz>
2020-11-06 18:03:02 +0600

    feat(author): created service for communication with mongo

--- libs/feature-author/src/lib/feature-author.module.ts
+++ libs/feature-author/src/lib/feature-author.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { AuthorSchema } from 'libs/schemas/author.schema';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthorsController } from './controllers/authors.controller';
+import { AuthorsService } from './services/authors.service';
 
 @Module({
-  controllers: [],
-  providers: [],
-  exports: [],
+  imports: [
+    MongooseModule.forFeature([{ name: 'author', schema: AuthorSchema }]),
+  ],
+  controllers: [AuthorsController],
+  providers: [AuthorsService],
 })
 export class FeatureAuthorModule {}
---
+++ libs/feature-author/src/lib/services/authors.service.ts
@@ -0,0 +1,28 @@
+import { Model } from 'mongoose';
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Author, AuthorDocument } from '../../../../schemas/author.schema';
+import { CreateAuthorDTO } from '../dtos/create-author.dto';
+import { ObjectId } from 'mongodb';
+
+@Injectable()
+export class AuthorsService {
+  constructor(@InjectModel('author') private authorModel: Model<AuthorDocument>) {}
+
+  async create(createAuthorDto: CreateAuthorDTO): Promise<Author> {
+    const createdAuthor = new this.authorModel(createAuthorDto);
+    return createdAuthor.save();
+  }
+
+  async findAll(): Promise<Author[]> {
+    return this.authorModel.find().exec();
+  }
+
+  async find(id: ObjectId): Promise<Author> {
+    return this.authorModel
+      .findOne({
+        _id: id,
+      })
+      .exec();
+  }
+}
