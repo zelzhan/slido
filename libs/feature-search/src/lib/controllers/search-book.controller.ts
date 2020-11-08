6444c70dd7b4ca927973dfec01c38d8431799856
zelzhan <elzhan.zeinulla@nu.edu.kz>
2020-11-08 17:56:10 +0600

    feat(search): added book search service indexing

--- libs/feature-search/src/lib/controllers/search-book.controller.ts
+++ libs/feature-search/src/lib/controllers/search-book.controller.ts
@@ -1,9 +1,10 @@
 import { Controller, Get, Query } from '@nestjs/common';
-import { Search } from '../dtos/Search.dto';
+import { Search } from '../dtos/search.dto';
+import { BooksSearchService } from '../services/search-author.service';
 
 @Controller('search/book')
 export class SearchBookController {
-  constructor(private service: BookSearchService) {}
+  constructor(private service: BooksSearchService) {}
 
   @Get()
   async create(@Query() { search }: Search) {
